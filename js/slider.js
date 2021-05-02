6bf2c9c0df39dc8bc66df0678e4da6aca2bf6fe0
ahmadecommerce <ahmadecommerce@gmail.com>
2021-05-02 04:51:14 +0300

    website

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,14 @@
 var $links =$(".itemLinks");
 $links.click(function(e){
     $links.removeClass("active");
-    var clickedlink =e.target;
-    clickedLink = $(clickedLink);
+    var clickedlink = e.target;
+    clickedlink = $(clickedlink);
     var position= clickedlink.attr("data-pos");
-    var translateValue="translate3d("+ position *25 +"%,0px ,0px)";
+    var translateValue = "translate3d("+ position * 25 +"%, 0px, 0)";
     $("#wrapper").css({
         transform:translateValue
     });
     clickedlink.addClass("active");
 });
-$(link[0]).addClass("active");
+
+$($links[0]).addClass("active");
